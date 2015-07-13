9c6c2d7fb0202885ba27bf1931fa58c6083102b4
FunnyQ <funnyq@funnyq.com>
2015-07-13 18:40:34 +0800

    [update] update capistrano to newer version

--- qstyle.rb
+++ qstyle.rb
@@ -56,7 +56,7 @@ gem_group :development, :test do
   gem 'bullet'
 
   # Deploy 工具
-  gem 'capistrano', '~> 3.1.0'
+  gem 'capistrano', '~> 3.4.0'
   gem 'capistrano-bundler', '~> 1.1.2'
   gem 'capistrano-rails', '~> 1.1.1'
   gem 'capistrano-rbenv', github: "capistrano/rbenv"
